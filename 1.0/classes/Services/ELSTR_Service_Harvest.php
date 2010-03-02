a3108ca9be69c766c533dff99458da614f1c8890
Marco Egli <eglinetwork@users.noreply.github.com>
2010-03-02 15:39:32 +0000

    Added Harvest Enterprise Application and Service

---
+++ 1.0/classes/Services/ELSTR_Service_Harvest.php
@@ -0,0 +1,41 @@
+<?php
+require_once ('ELSTR_Service_Abstract.php');
+require_once ('ELSTR_HttpClient.php');
+
+/**
+ * This as an example of a custom application, using http authentication
+ *
+ * @author Marco Egli
+ */
+class ELSTR_Service_Harvest extends ELSTR_Service_Abstract {
+
+    private $m_url;
+
+    /**
+     *
+     * @return
+     */
+    function __construct($options) {
+        parent::__construct();
+
+        $this->m_username = $options['username'];
+        $this->m_password = $options['password'];
+        $this->m_url = $options['url'];
+
+    }
+
+	protected function get($serviceUrl, $parameters) {
+			$sessionAuthAlfresco = new Zend_Session_Namespace('Auth_Alfresco');
+			$restClient = new ELSTR_HttpClient();
+	
+			$restClient->setUri($this->m_url.$serviceUrl);
+			$restClient->setHeaders('Accept', 'application/xml');
+			$authenticationString = "Basic (". base64_encode($this->m_username.':'.$this->m_password) .")";
+			$restClient->setHeaders('Authorization', $authenticationString);
+			
+			$restClient->setParameterGet($parameters);
+			return $restClient->request('GET');
+	}
+
+}
+?>
