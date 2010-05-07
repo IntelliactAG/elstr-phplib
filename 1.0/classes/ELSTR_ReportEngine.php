418dbc1c58cc595a630b27ef269eb1c9cc218e07
MartinBichsel <MartinBichsel@5c050769-0c6e-44ed-8073-5ecc4111bc47>
2010-05-07 06:15:51 +0000

    Supporting more than one sheet.

--- 1.0/classes/ELSTR_ReportEngine.php
+++ 1.0/classes/ELSTR_ReportEngine.php
@@ -6,7 +6,7 @@ $phpExcelOptions = $application->getOption("phpExcel");
 set_include_path(APPLICATION_PATH . '/phplib/phpExcel/' . $phpExcelOptions["version"] . '/Classes/'.PATH_SEPARATOR.get_include_path());
 
 /** PHPExcel */
-include 'PHPExcel.php';
+include_once 'PHPExcel.php';
 
 
 
@@ -23,6 +23,7 @@ class ELSTR_ReportEngine {
 	var $m_title;
 	var $m_autoColWidth;
 	var $m_file;
+	var $m_currentSheet;
 	
 
 	/**
@@ -34,9 +35,15 @@ class ELSTR_ReportEngine {
 	 */
 	function __construct($fileName = '',$excelVersion='')
 	{
-		$fullFileName = APPLICATION_PATH.'/application/reportTemplates/'.$fileName;
+		if (strpos($fileName,'/')>0)
+		{
+			$fullFileName = $fileName;
+		}
+		else
+		{
+			$fullFileName = APPLICATION_PATH.'/application/reportTemplates/'.$fileName;
+		}
 		$this->m_autoColWidth = false;
-		
 		if ($excelVersion =='') {
 			// FIXME
 			if (strpos($fullFileName,'.xlsx')>0) {
@@ -49,19 +56,19 @@ class ELSTR_ReportEngine {
 
 		if ($excelVersion=='Excel5')
 		{
-			include "PHPExcel/Reader/Excel5.php";
+			include_once "PHPExcel/Reader/Excel5.php";
 			$objReader = new PHPExcel_Reader_Excel5();
 			$this->m_objPHPExcel  = $objReader->load($fullFileName);
 		}
 		else if ($excelVersion=='Excel2003XML')
 		{
-			include "PHPExcel/Reader/Excel2003XML.php";
+			include_once "PHPExcel/Reader/Excel2003XML.php";
 			$objReader = new PHPExcel_Reader_Excel2003XML();
 			$this->m_objPHPExcel  = $objReader->load($fullFileName);
 		}
 		else if ($excelVersion=='Excel2007')
 		{
-			include "PHPExcel/Reader/Excel2007.php";
+			include_once "PHPExcel/Reader/Excel2007.php";
 			$objReader = new PHPExcel_Reader_Excel2007();
 			$this->m_objPHPExcel  = $objReader->load($fullFileName);
 		}
@@ -76,7 +83,36 @@ class ELSTR_ReportEngine {
 		$this->m_file = tempnam(sys_get_temp_dir(),"rep");
 	}
 
-
+	
+	/**
+	 * Sets current sheet for adding elements
+	 *
+	 * @param integer $currentSheet
+	 * @return void
+	 */
+	public function setSheetNumber($currentSheet)
+	{
+		$this->m_currentSheet = $currentSheet;
+	}
+	
+	/**
+	 * Get current sheet
+	 *
+	 * @return void
+	 */
+	private function getSheet($sheetNumber)
+	{
+		if (isset($sheetNumber))
+		{
+			return $this->m_objPHPExcel->getSheet($sheetNumber);
+		}
+		else
+		{
+			return $this->m_objPHPExcel->getActiveSheet();
+		}
+	}
+	
+	
 	/**
 	 * Add a table, consisting of columns(header) and data
 	 *
@@ -85,7 +121,7 @@ class ELSTR_ReportEngine {
 	 * @param string $cellSpecification, 'byColumnKey' or a specification of the topleft cell in the format according to locateReportElement
 	 * @return void
 	 */
-	public function addTable($columns,$data,$cellSpecification='byColumnKey'){
+	public function addTable($columns,$data,$cellSpecification='byColumnKey',$numHeaderRows=1){
 		$table=array();
 
 		//determine location of the head element of each column
@@ -120,6 +156,8 @@ class ELSTR_ReportEngine {
 			$table['columns'] = $columns;
 			$table['data'] = $data;
 			$table['type'] = 'table';
+			$table['sheet'] = $this->m_currentSheet;
+			$table['numHeaderRows'] = $numHeaderRows;
 			$this->m_reportElements[] = $table;
 			return true;
 		}
@@ -146,6 +184,7 @@ class ELSTR_ReportEngine {
 		{
 			$cell['value'] = $value;
 			$cell['type'] = 'cell';
+			$cell['sheet'] = $this->m_currentSheet;
 			$this->m_reportElements[] = $cell;
 			return true;
 		}
@@ -165,7 +204,7 @@ class ELSTR_ReportEngine {
 	public function addValues($values){
 		foreach ($values as $key => $value)
 		{
-		   $this->addValue($value, '{'.$key.'Cell}');
+		   $this->addValue($value, '{'.$key.'_cell}');
 		}
 	}
 
@@ -177,20 +216,21 @@ class ELSTR_ReportEngine {
 	 * @param string $cellSpecification either simple cell coordinate (e.g A1) or cell value starting with a non-alphabetic character (e.g. $name), to be located
 	 * @return array $reportElement including row and column if the element could be located
 	 */
-	private function locateReportElement($reportElement,$cellSpecification) {
+	public function locateReportElement($reportElement,$cellSpecification) {
 		//echo "locateReportElement $cellSpecification\n";
 		$prefix = substr($cellSpecification,0,5);
+		$sheet = $this->getSheet($this->m_currentSheet);
 		if ($prefix=='cell:') // assuming simple address, e.g. A1
 		{
 			$specification = substr($cellSpecification,5);
-			$excelCell = $this->m_objPHPExcel->getActiveSheet()->getCell($specification);
+			$excelCell = $sheet->getCell($specification);
 			if (!$excelCell) { return; }
 			$reportElement['col'] = PHPExcel_Cell::columnIndexFromString( $excelCell->getColumn() )-1;
 			$reportElement['row'] = $excelCell->getRow();
 		}
 		else
 		{
-			$rowIterator = $this->m_objPHPExcel->getActiveSheet()->getRowIterator();
+			$rowIterator = $sheet->getRowIterator();
 			foreach ($rowIterator as $row) {
 				$cellIterator = $row->getCellIterator();
   				$cellIterator->setIterateOnlyExistingCells(true);
@@ -249,9 +289,10 @@ class ELSTR_ReportEngine {
 	private function setCellData($reportElement)
 	{
 		$value = $reportElement['value'];
-		$col =     $reportElement['col'];
-		$row =     $reportElement['row'];
-		$cell = $this->m_objPHPExcel->getActiveSheet()->GetCellByColumnAndRow($col,$row);
+		$col =   $reportElement['col'];
+		$row =   $reportElement['row'];
+		$sheet = $this->getSheet($reportElement['sheet']);
+		$cell = $sheet->GetCellByColumnAndRow($col,$row);
 		$cell->setValue($value);
 	}
 
@@ -266,10 +307,11 @@ class ELSTR_ReportEngine {
 	{
 		$columns = $reportElement['columns'];
 		$data    = $reportElement['data'];
+		$numHeaderRows= $reportElement['numHeaderRows'];
 		$numRow  = count($data);
-		$sheet = $this->m_objPHPExcel->getActiveSheet();
+		$sheet = $this->getSheet($reportElement['sheet']);
 		
-		// Write the header row
+		// Prepare and Write the header row
 		for ($i = 0; $i < count($columns); $i++) {
 
 			$column = $columns[$i];
@@ -280,12 +322,15 @@ class ELSTR_ReportEngine {
 				$key = $column['key'];
 				$keyColumn[$key] = $i;
 				
-				$valueBelowTable = $sheet->GetCellByColumnAndRow($col,$row+2)->getValue();
-				$sheet->SetCellValueByColumnAndRow($col,$row+$numRow+1, $valueBelowTable);
+				$valueBelowTable = $sheet->GetCellByColumnAndRow($col,$row+$numHeaderRows+1)->getValue();
+				$sheet->SetCellValueByColumnAndRow($col,$row+$numRow+$numHeaderRows, $valueBelowTable);
 
 				$label = $columns[$i]['label'];
-				$sheet->SetCellValueByColumnAndRow($col,$row, $label);
-
+				if ($numHeaderRows>0)
+				{
+					$sheet->SetCellValueByColumnAndRow($col,$row, $label);
+				}
+					
 				if (isset($column['width']) && $this->m_autoColWidth){
 					$width = (integer) $column['width'];
 					$sheet->getColumnDimension(PHPExcel_Cell::stringFromColumnIndex($col))->setWidth($width/5);
@@ -296,7 +341,7 @@ class ELSTR_ReportEngine {
 		// Write the data rows
 		for ($j = 0; $j < count($data); $j++) {
 			$rowData = $data[$j];
-
+			
 			$keyArray = array_keys($rowData);
 			for ($i = 0; $i < count($keyArray); $i++) {
 				$key = $keyArray[$i];
@@ -305,12 +350,12 @@ class ELSTR_ReportEngine {
 					$value = $rowData[$key];
 					$iCol = $keyColumn[$key];
 					$column = $columns[$iCol];
-
+					
 					if(isset($column['col'])){
 
 						$col = $column['col'];
-						$row = $column['row']+ 1 + $j;
-
+						$row = $column['row']+ $numHeaderRows + $j;
+						
 						$cell = $sheet->GetCellByColumnAndRow($col,$row);
 						$cell->setValue($value);
 					}
@@ -328,13 +373,15 @@ class ELSTR_ReportEngine {
 	 */
 	private function setTableBodyStyle($reportElement)
 	{
-		$sheet = $this->m_objPHPExcel->getActiveSheet();
+		$sheet = $this->getSheet($reportElement['sheet']);
 		$columns = $reportElement['columns'];
 		$data    = $reportElement['data'];
 		$numRow  = count($data);
 		$headerRow = $reportElement['row'];
 		$rowHeight = $sheet->getRowDimension($headerRow)->getRowHeight();
 		$nextRowHeight = $sheet->getRowDimension($headerRow+1)->getRowHeight();
+		$numHeaderRows= $reportElement['numHeaderRows'];
+		
 		for ($i = 0; $i < count($columns); $i++) {
 
 			$column = $columns[$i];
@@ -343,47 +390,63 @@ class ELSTR_ReportEngine {
 				$col = $column['col'];
 				$row = $column['row'];
 	
-				$coordinate1 = PHPExcel_Cell::stringFromColumnIndex($col).($row+1);
-				$coordinate2 = PHPExcel_Cell::stringFromColumnIndex($col).($row+$numRow);
-				$coordinate3 = PHPExcel_Cell::stringFromColumnIndex($col).($row+$numRow+1);
-				$outsideStyle = $sheet->getStyleByColumnAndRow($col,$row+2);
+				$coordinate1 = PHPExcel_Cell::stringFromColumnIndex($col).($row+$numHeaderRows);
+				$coordinate2 = PHPExcel_Cell::stringFromColumnIndex($col).($row+$numRow-1+$numHeaderRows);
+				$coordinate3 = PHPExcel_Cell::stringFromColumnIndex($col).($row+$numRow+$numHeaderRows);
+				$outsideStyle = $sheet->getStyleByColumnAndRow($col,$row+1+$numHeaderRows);
 				$sheet->duplicateStyle ($outsideStyle,"$coordinate3:$coordinate3");
 				if ($numRow>0)
 				{
-				    $style = $sheet->getStyleByColumnAndRow($col,$row+1);
+				    $style = $sheet->getStyleByColumnAndRow($col,$row+$numHeaderRows);
 					$sheet->duplicateStyle ($style,"$coordinate1:$coordinate2");
 				}
 			}
 		}
-		for ($j = $headerRow+1; $j < $headerRow+1+$numRow; $j++) {
+		for ($j = $headerRow+$numHeaderRows; $j < $headerRow+$numHeaderRows+$numRow; $j++) {
 			$sheet->getRowDimension($j)->setRowHeight($rowHeight); // for unknown reasons row heights get screwed up during cell manipulation, reseting them here
 		}
-		$sheet->getRowDimension($headerRow+1+$numRow)->setRowHeight($rowHeight); // for unknown reasons row heights get screwed up during cell manipulation, reseting them here
+		$sheet->getRowDimension($headerRow+$numHeaderRows+$numRow)->setRowHeight($rowHeight); // for unknown reasons row heights get screwed up during cell manipulation, reseting them here
 	}
 
+	public function getColumnsForNames($columnNames){
+		$columns = array();
+		foreach ($columnNames as $columnName)
+		{
+			$column = array();
+			$column['key']=$columnName;
+			$column['label']=$columnName;
+			$columns[]=$column;
+		}
+		return $columns;
+	}
+	
 	
 	/**
 	 * Get the file stream of the report
 	 *
-	 * @param string $type (xlsx,pdf)
+	 * @param string $type (xlsx,xls,pdf)
+	 * @param string $pdfFfont one of the supported pdf fonts, requires 
 	 * @return stream
 	 */
-	public function getFile($type){
+	public function getFile($type,$pdfFont= 'freesans'){
 
 		switch ($type) {
 			case "pdf":
 				/** PHPExcel_Writer_PDF */
-				include 'PHPExcel/Writer/PDF.php';
+				include_once 'PHPExcel/Writer/PDF.php';
 				$objWriter = new PHPExcel_Writer_PDF($this->m_objPHPExcel);
+				$objWriter->writeAllSheets();
+				$objWriter->save($this->m_file,$pdfFont);
+				return $this->m_file;
 				break;
 			case "xls":
 				/** PHPExcel_Writer_Excel5 */
-				include 'PHPExcel/Writer/Excel5.php';
+				include_once 'PHPExcel/Writer/Excel5.php';
 				$objWriter = new PHPExcel_Writer_Excel5($this->m_objPHPExcel);
 				break;
 			default:
 				/** PHPExcel_Writer_Excel2007 */
-				include 'PHPExcel/Writer/Excel2007.php';
+				include_once 'PHPExcel/Writer/Excel2007.php';
 				$objWriter = new PHPExcel_Writer_Excel2007($this->m_objPHPExcel);
 		}
 
