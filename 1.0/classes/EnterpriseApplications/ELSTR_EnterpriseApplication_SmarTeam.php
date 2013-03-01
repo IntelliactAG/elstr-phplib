4546e20cb4e5ff4dc16880aefe93862cab1a989d
Felix Nyffenegger <nyfelix@users.noreply.github.com>
2013-03-01 14:40:27 +0000

    Added Support for SmarTeam

---
+++ 1.0/classes/EnterpriseApplications/ELSTR_EnterpriseApplication_SmarTeam.php
@@ -0,0 +1,40 @@
+<?php
+    require_once('ELSTR_EnterpriseApplication_Acl_Abstract.php');
+    require_once('Services/ELSTR_Service_SmarTeam_SOAP.php');
+
+    /**
+     * This as an example of a custom application, using http authentication
+     *
+     * @author Marco Egli
+     */
+    class ELSTR_EnterpriseApplication_SmarTeam extends ELSTR_EnterpriseApplication_Acl_Abstract
+    {
+
+        /**
+         * Required implementation of abstract method
+         * Register the services offered by this application
+         *
+         * @return
+         */
+        protected function _initServices() {
+        	$options = $this->m_application->getOption(get_class($this));
+        	$this->registerService(new ELSTR_Service_SAP_SOAP($options));
+        }
+        
+        public function call($service, $method){
+	        $args = func_get_args();
+	        	
+			if(PHP_VERSION_ID >= 50300){
+				// For PHP Version >= 5.3.0
+				// for PHP 5.3 we should write as follows (?) Ref: http://us2.php.net/manual/en/function.call-user-func-array.php
+				$response = call_user_func_array('parent::call', $args);
+			} else {
+				// For PHP Version < 5.3.0
+				$response = call_user_func_array(array($this, 'parent::call'), $args);
+			}
+			
+			return $response;
+        }
+
+    }
+?>
---
+++ 1.0/classes/Services/ELSTR_Service_SmarTeam_SOAP.php
@@ -0,0 +1,55 @@
+<?php
+require_once ('ELSTR_Service_Abstract.php');
+require_once ('ELSTR_SoapClient.php');
+
+/**
+ * This as an example of a custom application, using http authentication
+ *
+ * @author Marco Egli
+ */
+class ELSTR_Service_SmarTeam_SOAP extends ELSTR_Service_Abstract {
+
+    private $m_wsdl;
+    
+    private $m_username;
+    private $m_password;
+
+    /**
+     *
+     * @return
+     */
+    function __construct($options) {
+        parent::__construct();
+
+        $this->m_wsdl = $options['wsdl'];
+        
+        $this->m_login = $options['login'];
+        $this->m_password = $options['password'];
+
+    }
+
+    /**
+    * Call a SAP Soap Request
+    *
+    * @param string $method (the sap-method to call)
+    * @param array $parameters
+    * @return object
+    */
+	protected function request($method, $parameters) {
+	
+		// Set the soap options
+		$soapOptions = array('login' => $this->m_login,
+							'password' => $this->m_password,
+							'soap_version' => SOAP_1_1);
+		
+		// Create the soap client
+		$client = new Zend_Soap_Client($this->m_wsdl,$soapOptions);
+		$soapResponse = call_user_func_array(array($client, $method), array($parameters));
+	
+		return $soapResponse;
+			
+	}
+	
+
+}
+?>
