a84828749f96955d00f1dbe521e6e6e7872f7343
Marco Egli <eglinetwork@users.noreply.github.com>
2014-11-03 18:44:29 +0000

    Update for future compatibility

--- 1.0/classes/ELSTR_InitApplication.php
+++ 1.0/classes/ELSTR_InitApplication.php
@@ -24,37 +24,38 @@ if (isset($configPublic[APPLICATION_NAME]['libs'])) {
 }
 
 define('APPLICATION_VERSION', $configPublic['libs']['appVersion']);
-$yuiVersion = $configPublic['libs']['yuiVersion'];
 $elstrVersion = $configPublic['libs']['elstrVersion'];
 
 $elstrHeader = "";
-$elstrHeader .= "<script  type='text/javascript'>\n";
-$elstrHeader .= "LIBS = " . Zend_Json::encode($configPublic['libs']) . ";\n";
-$elstrHeader .= "LIBS.appName = '" . APPLICATION_NAME . "';\n";
+$elstrHeader .= "<script  type='text/javascript'>".PHP_EOL;
+$elstrHeader .= "LIBS = " . Zend_Json::encode($configPublic['libs']) . ";".PHP_EOL;
+$elstrHeader .= "LIBS.appName = '" . APPLICATION_NAME . "';".PHP_EOL;
 //if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['api'])) {
-//    $elstrHeader .= "API = " . Zend_Json::encode($_GET) . ";\n";
+//    $elstrHeader .= "API = " . Zend_Json::encode($_GET) . ";".PHP_EOL;
 //}
-$elstrHeader .= "</script>\n";
+$elstrHeader .= "</script>".PHP_EOL;
 
 if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($isApiRequest) && $isApiRequest == true) {
-    $elstrHeader .= "<script  type='text/javascript'>\n";
-    $elstrHeader .= "API = " . Zend_Json::encode($apiParameters) . ";\n";
-    $elstrHeader .= "</script>\n";
+    $elstrHeader .= "<script  type='text/javascript'>".PHP_EOL;
+    $elstrHeader .= "API = " . Zend_Json::encode($apiParameters) . ";".PHP_EOL;
+    $elstrHeader .= "</script>".PHP_EOL;
     $elstrHeader .= "<base href='" . $apiBase . "' />";
 } elseif ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['api'])) {
     // Needed for backward compatibility where an api call was redirected
-    $elstrHeader .= "<script  type='text/javascript'>\n";
-    $elstrHeader .= "API = " . Zend_Json::encode($_GET) . ";\n";
-    $elstrHeader .= "</script>\n";
+    $elstrHeader .= "<script  type='text/javascript'>".PHP_EOL;
+    $elstrHeader .= "API = " . Zend_Json::encode($_GET) . ";".PHP_EOL;
+    $elstrHeader .= "</script>".PHP_EOL;
 }
 
 // Load the correct YUI-Seedfile
 if (strpos($elstrVersion, "1.") === 0) {
-    $elstrHeader .= "<script type='text/javascript' src='jslib/yui/" . $yuiVersion . "/build/yuiloader/yuiloader-min.js' ></script>\n";
+    $yuiVersion = $configPublic['libs']['yuiVersion'];
+    $elstrHeader .= "<script type='text/javascript' src='jslib/yui/" . $yuiVersion . "/build/yuiloader/yuiloader-min.js' ></script>".PHP_EOL;
     $application->getBootstrap()->getResource("language")->cleanup();
-} else {
+} elseif(strpos($elstrVersion, "2.") === 0) {
     // Load the YUI3 used with elstr 2.0 on frontend
-    $elstrHeader .= "<script type='text/javascript' src='jslib/yui/" . $yuiVersion . "/build/yui/yui-min.js' ></script>\n";
+    $yuiVersion = $configPublic['libs']['yuiVersion'];
+    $elstrHeader .= "<script type='text/javascript' src='jslib/yui/" . $yuiVersion . "/build/yui/yui-min.js' ></script>".PHP_EOL;
     $application->getBootstrap()->getResource("language")->cleanup();
     if (isset($languageModulesToRegister)) {
         $application->getBootstrap()->getResource("language")->registerModules($languageModulesToRegister);
@@ -62,12 +63,12 @@ if (strpos($elstrVersion, "1.") === 0) {
     }
     require_once ('ELSTR_ApplicationDataServer.php');
     $applicationDataServer = new ELSTR_ApplicationDataServer($application);
-    $elstrHeader .= "<script  type='text/javascript'>\n";
-    $elstrHeader .= "ELSTR = {\n";
-    $elstrHeader .= "    applicationData : " . Zend_Json::encode($applicationDataServer->load(APPLICATION_NAME)) . ",\n";
-    $elstrHeader .= "    modules : " . file_get_contents(APPLICATION_PATH . "/public/jslib/elstr/" . $configPublic['libs']['elstrVersion'] . "/build/modules.txt") . "\n";
-    $elstrHeader .= "}\n";
-    $elstrHeader .= "</script>\n";
+    $elstrHeader .= "<script  type='text/javascript'>".PHP_EOL;
+    $elstrHeader .= "ELSTR = {".PHP_EOL;
+    $elstrHeader .= "    applicationData : " . Zend_Json::encode($applicationDataServer->load(APPLICATION_NAME)) . ",".PHP_EOL;
+    $elstrHeader .= "    modules : " . file_get_contents(APPLICATION_PATH . "/public/jslib/elstr/" . $configPublic['libs']['elstrVersion'] . "/build/modules.txt") . PHP_EOL;
+    $elstrHeader .= "}".PHP_EOL;
+    $elstrHeader .= "</script>".PHP_EOL;
 }
 
 $elstrHeader .= "<script type='text/javascript' src='" . APPLICATION_VERSION . "/" . APPLICATION_NAME . "/" . APPLICATION_NAME . ".js' ></script>";
