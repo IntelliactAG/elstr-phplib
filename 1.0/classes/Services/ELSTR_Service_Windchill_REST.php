21d01f3dd46b1baba7f3b4c737169e9f7dd1bb27
Marco Egli <eglinetwork@users.noreply.github.com>
2015-03-03 06:29:46 +0000

    Added persistent (cached) session with a cookie jar

--- 1.0/classes/Services/ELSTR_Service_Windchill_REST.php
+++ 1.0/classes/Services/ELSTR_Service_Windchill_REST.php
@@ -16,6 +16,8 @@ class ELSTR_Service_Windchill_REST extends ELSTR_Service_Abstract {
     private $m_username;
     private $m_password;
 
+    private $m_cookieJarCache = null;
+
     /**
      *
      * @return
@@ -30,21 +32,55 @@ class ELSTR_Service_Windchill_REST extends ELSTR_Service_Abstract {
         $this->m_username = $options['username'];
         $this->m_password = $options['password'];
 
+        if(isset($options['cookieJarCache'])){
+            $this->m_cookieJarCache = $options['cookieJarCache'];
+        }
+        
     }
 
 	protected function request($serviceUrl, $parameters) {
-			
-			$restClient = new ELSTR_HttpClient();
-			$restClient->setAuth($this->m_username, $this->m_password);
-			$restClient->setUri($this->m_url.$serviceUrl);
-			$restClient->setConfig(array(
-			'maxredirects' => $this->m_maxredirects,
-			'timeout'      => $this->m_timeout));			
-			$restClient->setParameterGet($parameters);
-            $response = $restClient->request();
-            //print_r($restClient->getLastRequest());
-			return $response;
-	}	
+
+		$restClient = new ELSTR_HttpClient();
+		$restClient->setAuth($this->m_username, $this->m_password);
+		$restClient->setUri($this->m_url.$serviceUrl);
+		$restClient->setConfig(array(
+		'maxredirects' => $this->m_maxredirects,
+		'timeout'      => $this->m_timeout));			
+		$restClient->setParameterGet($parameters);
+
+        if($this->m_cookieJarCache) {
+            $last_char = substr(sys_get_temp_dir(), -1, 1);
+            $systemTempDir = sys_get_temp_dir();
+            //last char might be \n => account for that
+            if (strpos($last_char, "/")===false || strpos($last_char, "\\")===false) {
+                $systemTempDir .= "/";
+            }
+            $systemTempDir .= "ELSTR_Service_Windchill_REST/";
+            if (!file_exists($systemTempDir)){
+                mkdir($systemTempDir);
+            }
+
+            $cache = Zend_Cache::factory('Core','File',$this->m_cookieJarCache,array('cache_dir' => $systemTempDir));            
+            if(!$cookieJar = $cache->load('cookieJar_'.Zend_Session::getId())) {
+                // Cache miss; 
+                $restClient->setCookieJar();
+            } else {
+                $restClient->setCookieJar($cookieJar);
+            }
+        }
+
+        $response = $restClient->request();
+        //print_r($restClient->getLastRequest());
+
+        if($this->m_cookieJarCache) {
+            if ($restClient->getCookieJar() !== $cookieJar){
+                $cache->save($restClient->getCookieJar(),'cookieJar_'.Zend_Session::getId());
+            }
+        }
+
+        
+		return $response;
+	}
 
 }
 ?>
